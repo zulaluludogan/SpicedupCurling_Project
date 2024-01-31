dda56a78ca4d99e0dbda25df86beded1bc78444e
zulaluludogan <zulaluludogan01@gmail.com>
2024-01-31 17:14:55 +0300

    new videos added

--- findHsvValue.py
+++ findHsvValue.py
@@ -1,24 +1,41 @@
 import cv2
+import numpy as np
 from cvzone.ColorModule import ColorFinder
 
-# cap = cv2.VideoCapture('video1.mp4')
-cap = cv2.VideoCapture(0)
-# frameCounter = 0 
+cap = cv2.VideoCapture('video2.mp4')
+# cap = cv2.VideoCapture(0)
+frameCounter = 0 
 
-# colorFinder = ColorFinder(True) # To Decide HSV values of objects "True"
+colorFinder = ColorFinder(True) # To Decide HSV values of objects "True"
+cornerPoints = [[92,27],[11,545],[457,87],[376,600]]
+
+def getBoard(img):
+    width, height = int(297*2.5),int(210*2.5)  # A4 paper size will be changed for the board size (height = 600 mm)
+
+    pts1 =  np.float32(cornerPoints)
+    pts2 =  np.float32([[0,0],[width,0],[0,height],[width,height]])
+    matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    imgOutput = cv2.warpPerspective(img,matrix,(width, height))
+
+    return imgOutput
 
 while True:
-    # frameCounter += 1
-    # if frameCounter == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-    #     frameCounter = 0 
-    #     cap.set(cv2.CAP_PROP_POS_FRAMES,0)
+    frameCounter += 1
+    if frameCounter == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        frameCounter = 0 
+        cap.set(cv2.CAP_PROP_POS_FRAMES,0)
 
-    # success, img = cap.read()
-    # imgColor, mask = colorFinder.update(img)
+    success, img = cap.read()
+    imgBoard = getBoard(img)
 
-    # cv2.imshow("ImageColor",imgColor)
-    mm=cv2.imread('img.png')
-    cv2.imshow("hee",mm)
+    imgColor, mask = colorFinder.update(imgBoard)
+
+    
+
+    cv2.imshow("ImageColor",imgColor)
+    # cv2.imshow("Image",img)
+    # mm=cv2.imread('img.png')
+    # cv2.imshow("hee",mm)
     
     cv2.waitKey(1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import cv2
-import cvzone
+import time
 import numpy as np
 from cvzone.ColorModule import ColorFinder
-import pickle
 
-cap = cv2.VideoCapture('video1.mp4')
+
+cap = cv2.VideoCapture('video2.mp4')
 frameCounter = 0 
 
-cornerPoints = [[418,142],[418,683],[21,76],[24,752]]  # Calibrate acc board
+# cornerPoints = [[418,142],[418,683],[21,76],[24,752]]  # Calibrate acc board
+cornerPoints = [[92,27],[11,545],[457,87],[376,600]] # video2.py coordinates
 
 colorFinder = ColorFinder(False) # To Decide HSV values of objects "True"
 
 hsvTargetVals = {'hmin': 156, 'smin': 67, 'vmin': 0, 'hmax': 179, 'smax': 255, 'vmax': 255}  # Red Target
 hsvPuck1Vals = {'hmin': 26, 'smin': 63, 'vmin': 0, 'hmax': 58, 'smax': 255, 'vmax': 255}     # Yellow Puck
 hsvPuck2Vals = {'hmin': 56, 'smin': 97, 'vmin': 0, 'hmax': 111, 'smax': 255, 'vmax': 255}    # Blue Puck
-hsvObstacleVals = {'hmin': 0, 'smin': 25, 'vmin': 66, 'hmax': 179, 'smax': 50, 'vmax': 160}  # Black Obstacles
+# hsvObstacleVals = {'hmin': 0, 'smin': 25, 'vmin': 66, 'hmax': 179, 'smax': 50, 'vmax': 160}  # Black Obstacles #video1.py
+hsvObstacleVals = {'hmin': 0, 'smin': 0, 'vmin': 0, 'hmax': 179, 'smax': 18, 'vmax': 124}    # Black Obstacles #video2.py
 
 def getBoard(img):
     width, height = int(297*2.5),int(210*2.5)  # A4 paper size will be changed for the board size (height = 600 mm)
@@ -58,10 +60,29 @@ def findEdgePointsObst(cnt):
     cv2.drawContours(imgBoard,[box],0,(0,0,255),2)
 
     return box
-myTurn = 1
+
+def getFirstFrame(imgBoard):
+    maskFirstFrame = createHsvMask(imgBoard, hsvPuck1Vals)
+    
+    return maskFirstFrame
+
+def defineMyTurn(imgBoard):          # EDIT THIS PART FOR THE PUCK NEXT TO BOARD
+    global myTurn
+    maskPuck1 = createHsvMask(imgBoard, hsvPuck1Vals)
+    maskPuck2 = createHsvMask(imgBoard, hsvPuck2Vals)
+    _, numberofPunks1 =  detectContour(maskPuck1)
+    _, numberofPunks2 =  detectContour(maskPuck2)
+    if numberofPunks1 > numberofPunks2:
+        myTurn = 1
+        print(" You are starting!")
+    else:
+        myTurn = 0
+        print(" Opponent is starting!")
+     
+motionDetected = 0
+START = 1
 
 while True:
-    
     frameCounter += 1
     if frameCounter == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         frameCounter = 0 
@@ -70,16 +91,39 @@ while True:
     success, img = cap.read()
     imgBoard = getBoard(img)
 
-    maskPuck1 = createHsvMask(imgBoard, hsvPuck1Vals)
-    maskPuck2 = createHsvMask(imgBoard, hsvPuck2Vals)
+    if START :  # Define beginning conditions
+        maskFirstFrame = getFirstFrame(imgBoard)
+        defineMyTurn(imgBoard)
+        START = 0
+
     maskTarget = createHsvMask(imgBoard, hsvTargetVals)
     maskObst = createHsvMask(imgBoard, hsvObstacleVals)
+    maskPuck1 = createHsvMask(imgBoard, hsvPuck1Vals)
+    maskPuck2 = createHsvMask(imgBoard, hsvPuck2Vals)
 
     contTarget, numberofTarget =  detectContour(maskTarget)
     contObstac, numberofObstac =  detectContour(maskObst)
     contPuck1, numberofPunks1 =  detectContour(maskPuck1)
     contPuck2, numberofPunks2 =  detectContour(maskPuck2)
     
+    if not myTurn:  
+        # print( " ACTIVE PLAYER 2")
+        diff = cv2.absdiff(maskPuck1, maskFirstFrame)
+        threshold = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
+        maskFirstFrame = getFirstFrame(imgBoard)
+        if threshold.sum() > 50000:
+            print("---motion is detected")
+
+            # "wait until motion stops"
+            motionDetected = 1
+ 
+        elif motionDetected:
+            myTurn = 1
+            motionDetected = 0
+            print(">>>motion stopped")
+
+        cv2.imshow("threshold",threshold)
+
     if myTurn:
         obstacleEdgePoints = []
         Puck1CenterRadius = []
@@ -96,6 +140,21 @@ while True:
         for cont in contObstac:
             boxPoints = findEdgePointsObst(cont)  # 4 edge points of one rectangele obstacle
             obstacleEdgePoints.append(boxPoints)
+        
+        print("MY TURN!!!! RUN ALGORITHM")
+        print( "ROBOT TAKES AN ACTION")
+        
+        print("puck1",str(Puck1CenterRadius))
+        print("puck2",str(Puck2CenterRadius))
+        print("target", str(targetCenterRadius))
+        print("obstac", str(obstacleEdgePoints))
+        
+        time.sleep(2)
+        # WAIT A FEW SEC BEFORE GETTING FIRST FRAME
+
+        maskFirstFrame = getFirstFrame(imgBoard)
+        myTurn = 0
+        
     
     # print("puck1",str(Puck1CenterRadius))
     # print("puck2",str(Puck2CenterRadius))
@@ -111,6 +170,7 @@ while True:
 
 
     cv2.imshow("ImageBoard",imgBoard)
+   
     # cv2.imshow("Image",img)
     # cv2.imshow("ImageMask",maskPuck1)
     # cv2.imshow("ImageContours",imgContours)
